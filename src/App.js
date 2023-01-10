2d53688dbfae169a992ea37d1178222671b26a7f
Job Alvarez <jobitoalv@gmail.com>
2023-01-10 15:42:51 +1000

    creating pros for shopping card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [name, setName] = useState ([]);
+  const [name, setName] = useState ('');
+  const [list, setList] = useState ([]);
+  const [isEditing, setIsEditing] = useState(false);
 
   return <h2>grocery bud setup</h2>
 }
